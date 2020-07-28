54ff281965ba426ca55174c0039fee0fb53f9a65
Sandesh <66875657+theasksystem@users.noreply.github.com>
2020-07-29 03:09:14 +0530

    Store Search Implementation

--- ar/server.php
+++ ar/server.php
@@ -6,17 +6,25 @@ include('../include/db.class.php');
 if(isset($_POST['query'])){
 	$keyword = strval($_POST['query']);
 	$search_param = "{$keyword}%";
-
 	$sql = $conn->prepare("SELECT product_name_en FROM products WHERE status = '1' AND product_name_en LIKE '%$search_param%'");
-//	$sql->bind_param("s",$search_param);			
+	$sqlstore = $conn->prepare("SELECT id,company FROM tbl_admin WHERE company LIKE '%$search_param%'");
+	//	$sql->bind_param("s",$search_param);			
 	$sql->execute();
+	$sqlstore ->execute();
 	$result = $sql->fetch(PDO::FETCH_ASSOC);
+	$resultstore = $sql->fetch(PDO::FETCH_ASSOC);
+	
 
-	if ($sql->rowCount() > 0) {
+	if ($sql->rowCount() > 0 || $sqlstore->rowCount() > 0) {
 	    echo '<ul class="typeahead dropdown-menu" role="listbox" style="top: 40px; left: 0px; display: block;">';
 		while($row = $sql->fetch(PDO::FETCH_ASSOC)) {
-		$countryResult2 = $row["product_name_en"];
-		echo '<li class="active"><a class="dropdown-item" href="products?s='.$countryResult2.'" role="option">'.$countryResult2.'</a></li>';
+			$countryResult2 = $row["product_name_en"];
+			echo '<li class="active"><a class="dropdown-item" href="products?s='.$countryResult2.'" role="option">'.$countryResult2.'</a></li>';
+		}
+		while($row = $sqlstore->fetch(PDO::FETCH_ASSOC))	{
+			$countryResult2 = $row["company"];
+			$storeid = $row["id"];
+			echo '<li class="active"><a class="dropdown-item" href="store.php?own='.base64_encode(base64_encode(base64_encode($storeid))).'" role="option">'.$countryResult2.'</a></li>';
 		}
 		echo '</ul>';
 	}
